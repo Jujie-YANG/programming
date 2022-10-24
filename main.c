5aa53fef7b9cbcb46525f38d2c77662516e674c4
Jujie-YANG <Yangjujie3619@outlook.com>
2022-10-24 01:45:01 +0100

    running exmaple code during lessons

--- main.c
+++ main.c
@@ -1,7 +1,18 @@
+#include <stdlib.h>
 #include <stdio.h>
-
 int main()
 {
-    printf("Hello World!");
+    int x = 1;
+    int *p = &x;
+    int i = 1;
+    while (-1)
+    {
+        p++;
+        // write a value
+        *p = rand();
+        // *p = 1234;
+        printf("%d : so far so good. Content of %p is %d\n", i, p, *p);
+        i++;
+    }
     return 0;
 }
